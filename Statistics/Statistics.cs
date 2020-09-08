05b752bf54d3d6403bb1b0284b22690ec6c50ad4
AdarshSrivatsa98 <adarsh.98srivatsa@gmail.com>
2020-09-08 12:03:50 +0530

    update to find Statistics

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public StatStruct CalculateStatistics(List<float> numbers) {
+            StatStruct result = new StatStruct();
+            if (numbers.Capacity == 0)
+            {
+                result.average = Double.NaN;
+                result.max = Double.NaN;
+                result.min = Double.NaN;
+                return result;
+            }
+            result.average = numbers.Average();
+            result.min = numbers.Min();
+            result.max = numbers.Max();
+            return result;
         }
     }
 }
